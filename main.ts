7827c231a8cfe5a60fc4bb0b06cf758f6b3f27fb
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-03 09:23:19 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 basic.showString("hej")
 basic.forever(function () {
+    basic.clearScreen()
     for (let index = 0; index < 4; index++) {
-        basic.showNumber(0)
         basic.pause(100)
+        music.ringTone(262)
     }
 })
