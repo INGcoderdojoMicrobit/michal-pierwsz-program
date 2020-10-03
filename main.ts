02b9ddc06962601a11c0df4945a295feb5054e0f
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-03 09:21:16 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-basic.showIcon(IconNames.Heart)
+basic.showString("hej")
 basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        basic.showNumber(0)
+        basic.pause(100)
+    }
 })
