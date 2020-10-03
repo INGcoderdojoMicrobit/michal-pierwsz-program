d362f35117e84169b36b5ac1b00aa2b63277b15e
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-03 09:30:24 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Tortoise)
+})
 basic.showString("hej")
 basic.showNumber(0)
 basic.forever(function () {
     basic.clearScreen()
     for (let index = 0; index < 4; index++) {
+        basic.showString("tak")
         basic.showNumber(0)
         basic.pause(100)
-        music.ringTone(185)
     }
 })
